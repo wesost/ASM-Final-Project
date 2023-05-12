0825b12e6eef0a0d2960bf34b5f56ad80086c701
David Conklin <cconklin25@my.whitworth.edu>
2023-05-12 09:46:07 -0700

    Changes

--- source/scrapy.py
+++ source/scrapy.py
@@ -32,31 +32,31 @@ import struct
 
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# SCRAP FOR INFO:
-# ---------------
-edge_driver_path = "edgedriver_win64\msedgedriver.exe"                                                                   # set the path to the Edge driver executable file
+# SCRAPE FOR INFO:
+# ----------------
+# edge_driver_path = "edgedriver_win64\msedgedriver.exe"                                                                   # set the path to the Edge driver executable file
 
-# Set the options for running the browser in headless mode
-options = webdriver.EdgeOptions()
-options.add_argument('headless')                                                                                         # Stops the webpage window from appearing every time script runs
+# # Set the options for running the browser in headless mode
+# options = webdriver.EdgeOptions()
+# options.add_argument('headless')                                                                                         # Stops the webpage window from appearing every time script runs
 
-# Create an instance of the Edge driver with headless options
-driver = webdriver.Edge(executable_path=edge_driver_path, options=options)                                             
-driver.get("https://forecast.weather.gov/MapClick.php?x=210&y=120&site=otx&zmx=&zmy=&map_x=209&map_y=120#.ZElK5c7MLl0")  # navigate to a weather.gov spokane washingtons page
+# # Create an instance of the Edge driver with headless options
+# driver = webdriver.Edge(executable_path=edge_driver_path, options=options)                                             
+# driver.get("https://forecast.weather.gov/MapClick.php?x=210&y=120&site=otx&zmx=&zmy=&map_x=209&map_y=120#.ZElK5c7MLl0")  # navigate to a weather.gov spokane washingtons page
 
-# Wait for the temperature element to appear for a maximum of 4 seconds
-# Allows us to ensure webpage is loaded fully before we grab the info
-element = WebDriverWait(driver, 4).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "myforecast-current-lrg"))
-)
+# # Wait for the temperature element to appear for a maximum of 4 seconds
+# # Allows us to ensure webpage is loaded fully before we grab the info
+# element = WebDriverWait(driver, 4).until(
+#     EC.presence_of_element_located((By.CLASS_NAME, "myforecast-current-lrg"))
+# )
 
-# Extract some information from the website using Selenium
-todaysTemp = driver.find_element(By.CLASS_NAME, "myforecast-current-lrg")                                                # Looks for the current temp of Spokane in F
-todaysTemp_text = todaysTemp.text                                                                                        # Transfers that data into text for printing        
+# # Extract some information from the website using Selenium
+# todaysTemp = driver.find_element(By.CLASS_NAME, "myforecast-current-lrg")                                                # Looks for the current temp of Spokane in F
+# todaysTemp_text = todaysTemp.text                                                                                        # Transfers that data into text for printing        
 
-print("TEMP: ", todaysTemp_text)                                                                                         # Prints the data to terminal to see if it is correct
+# print("TEMP: ", todaysTemp_text)                                                                                         # Prints the data to terminal to see if it is correct
 
-driver.quit()                                                                                                            # close the browser window when done
+# driver.quit()                                                                                                            # close the browser window when done
 
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -69,20 +69,20 @@ time.sleep(2)                                                                  #
 # FORMAT TODAYS TEMP:
 # -------------------
 #  -> Gets TempText ready to be sent to chip to be processed
-hex_val = str(re.findall(r'\d+', todaysTemp_text)[0])           # extract the number part from todaysTemp_text
-byte_val = bytes.fromhex(hex_val)                               # Format tempreture into bytes
-print(f"About to send {byte_val.hex()} to Arduino")             # Print todaysTemp to the terminal - testing purposes
+hex_val =  random.randint(1,116)    #str(re.findall(r'\d+', todaysTemp_text)[0])           # extract the number part from todaysTemp_text
+# byte_val = bytes.fromhex(hex_val)                               # Format tempreture into bytes
+# print(f"About to send {byte_val.hex()} to Arduino")             # Print todaysTemp to the terminal - testing purposes
 
-todaysTempre = int(hex_val)
-temperatureValues = [24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116]
-count = 0
-#count_val = '00'
-for i in range(len(temperatureValues)):
-    if todaysTempre > temperatureValues[i]:
-        count += 1
-    elif todaysTempre < temperatureValues[i]:
-        print(count)  
-        count_val = struct.pack('B', count)  # Convert count to a single byte
+todaysTempre = int(hex_val)                                                                                                 # Var to hold todays temp from weather.gov
+print(todaysTempre)                                                                                                         # Print the value of todays temp to the terminal
+temperatureValues = [24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116]   # Assigns a value to each neopixel on the ring 
+count = 0                                                                                                                   # Set up a count varibale to keep track of amout of LEDs we want turned on
+for i in range(len(temperatureValues)):                                                                                     # Loop through every index in pixel array
+    if todaysTempre >= temperatureValues[i]:                                                                                # If todays temp is greater or equal to current index
+        count += 1                                                                                                          # Increase count by one
+    elif todaysTempre < temperatureValues[i]:                                                                               # If todays temp is less than the current index exit
+        print(count)                                                                                                        # Print count to terminal - testing purposes
+        count_val = struct.pack('B', count)                                                                                 # Convert count to a single byte
         break
 
 
@@ -122,7 +122,7 @@ if todaysWeather_text == "Clear":                   # Check if Clear today
     ser.write(clv)                                  
     time.sleep(2)                                   
     ser.write(count_val)                             
-    print(f"Sent {byte_val.hex()} to Arduino")      # Print value of temp - Testing purposes
+    # print(f"Sent {byte_val.hex()} to Arduino")      # Print value of temp - Testing purposes
 
 elif todaysWeather_text == "Overcast":              # Check if Overcast toady
     ser.write(msgSend)                              
@@ -130,7 +130,7 @@ elif todaysWeather_text == "Overcast":              # Check if Overcast toady
     ser.write(ocv)                                 
     time.sleep(2)                                  
     ser.write(count_val)                             
-    print(f"Sent {byte_val.hex()} to Arduino")      # Print value of temp - Testing purposes
+    # print(f"Sent {byte_val.hex()} to Arduino")      # Print value of temp - Testing purposes
 
 elif todaysWeather_text == "Showers":               # Check if Rainy
     ser.write(msgSend)
@@ -138,7 +138,7 @@ elif todaysWeather_text == "Showers":               # Check if Rainy
     ser.write(shv)
     time.sleep(2)
     ser.write(count_val)
-    print(f"Sent {byte_val.hex()} to Arduino")      # Print value of temp - Testing purposes
+    # print(f"Sent {byte_val.hex()} to Arduino")      # Print value of temp - Testing purposes
 
 elif todaysWeather_text == "T-storms":              # Check if Thunderin and Rumblin
     ser.write(msgSend)
@@ -146,6 +146,6 @@ elif todaysWeather_text == "T-storms":              # Check if Thunderin and Rum
     ser.write(tsv)
     time.sleep(2)
     ser.write(count_val)
-    print(f"Sent {byte_val.hex()} to Arduino")      # Print value of temp - Testing purposes
+    # print(f"Sent {byte_val.hex()} to Arduino")      # Print value of temp - Testing purposes
 ser.close()                                         # close the serial port
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
